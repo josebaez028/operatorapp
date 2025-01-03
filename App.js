b22ea43bf41205802945f335ba96741678350cdb
josebaez028 <josebaez028@gmail.com>
2025-01-03 17:58:39 -0400

    Add Stack Navigator

--- App.js
+++ App.js
@@ -3,19 +3,36 @@ import Splash from './src/screens/auth/Splash';
 import Signup from './src/screens/auth/Signup';
 import Config from 'react-native-config';
 import Signin from './src/screens/auth/Signin';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { colors } from './src/utils/colors';
+
+const Stack = createNativeStackNavigator();
+
+const theme = {
+  colors: {
+    background: colors.white,
+  }
+}
 
 export default function App() {
   const googleData = {
     webClientId: Config.GOOGLE_WEB_CLIENT_ID
   }
-  console.log(googleData)
+
   return (
-    <SafeAreaView>
+    // <SafeAreaView>
+    //   <Splash />
+    // </SafeAreaView>
       
-      {/* <Splash /> */}
-      {/* <Signup /> */}
-      <Signin />
-    </SafeAreaView>
+      <NavigationContainer theme = {theme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="Signin" component={Signin} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+      </NavigationContainer>
+   
     
   );
 }
